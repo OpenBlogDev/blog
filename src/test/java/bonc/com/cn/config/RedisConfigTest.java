d8f5ac0e01f2fb2db7f6af91f475448caffe4f28
whw <wanghongwei1@bonc.com>
2019-12-01 15:10:43 +0800

    提交

--- src/test/java/bonc/com/cn/config/RedisConfigTest.java
+++ src/test/java/bonc/com/cn/config/RedisConfigTest.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 // @Configuration
-public class RedisConfig {
+public class RedisConfigTest {
 
     @SuppressWarnings("deprecation")
     @Bean
