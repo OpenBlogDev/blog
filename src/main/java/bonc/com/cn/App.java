fa7c5405e97683ca560cbd60ac92cb0c31a4a3ce
whw <wanghongwei1@bonc.com>
2019-12-01 16:11:41 +0800

    提交测试

--- src/main/java/bonc/com/cn/App.java
+++ src/main/java/bonc/com/cn/App.java
@@ -1,10 +1,14 @@
 package bonc.com.cn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ThymeleafAutoConfiguration.class, FreeMarkerAutoConfiguration.class})
 @EnableCaching
 public class App {
 
