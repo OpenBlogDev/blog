07519791b127e5b9f131a5271c391dc68db21fe7
whw <wanghongwei1@bonc.com>
2019-12-05 16:25:24 +0800

    提交遗留的java

--- src/main/java/bonc/com/cn/config/IndexAndCROSAndInterceptor.java
+++ src/main/java/bonc/com/cn/config/IndexAndCROSAndInterceptor.java
@@ -1,16 +1,32 @@
 package bonc.com.cn.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import bonc.com.cn.Interceptor.MyInterceptor;
 
-
+/**
+ * 
+ * @author whw
+ *
+ */
 @Configuration
-public class CROSConfigAndInterceptor implements WebMvcConfigurer {
-
+public class IndexAndCROSAndInterceptor implements WebMvcConfigurer {
+	/**
+	 * @return 输入localhost:8080返回默认页面index.html
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+	/**
+	 * @return 设置跨域允许localhost:8081请求访问
+	 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/*")
@@ -19,15 +35,13 @@ public class CROSConfigAndInterceptor implements WebMvcConfigurer {
 			.maxAge(1800)
 			.allowedOrigins("http://localhost:8081");
 	}
-
+	/**
+	 * @return 拦截器设置拦截所有
+	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new MyInterceptor()).addPathPatterns("*").excludePathPatterns("*");
 	}
-	
-	
-	
-	
 	
 	
 
