c0c075ec6df610b8b3bc214129922ebe40c89ad4
whw <wanghongwei1@bonc.com>
2019-12-07 10:08:41 +0800

    使用路由实现现在流行的前后端分离

--- src/main/blogweb/src/App.vue
+++ src/main/blogweb/src/App.vue
@@ -1,37 +1,15 @@
 <template>
   <div id="app">
-      <headers></headers>
-      <contents></contents>
-      <!-- <footers></footers>      -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import headers from './components/header';
-import footers from './components/footer';
-import contents from './components/content'
 export default {
-  name: 'App',
-  components:{
-    'headers':headers,
-    'footers':footers,
-    'contents':contents,
-
-  }
-}
+  name: "App"
+};
 </script>
 <style>
-body{
-  background-color: #f5f7f9;
-}
-a{
-  color: #f5f7f9;
-}
-a:hover {
-  color: aqua;
-}
-</style>
-<style scoped>
-
 
 </style>
+<style scoped></style>
---
+++ src/main/blogweb/src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loginHeader">
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+<style scoped>
+.loginHeader{
+
+}
+</style>
